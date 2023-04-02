df223acc5af5d7e44d653905bd9d2b8f15858de8
unknown <bielcalaca12lol@gmail.com>
2023-04-01 22:33:12 -0300

    segundo commit

--- index.js
+++ index.js
@@ -11,8 +11,28 @@ app.set('view engine','ejs'); //dizendo para o express que a view engine é o ej
 
 
 
-app.get("/",function(req,res){
-    res.render("index");
+app.get("/:nome/:lang",function(req,res){
+
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+        {nome:"Costela",preco:30.50},
+        {nome:"linguica",preco:29.50},
+        {nome:"File",preco:20.50}
+    ];
+    
+
+    res.render("index",{
+        nome: nome,
+        lang: lang,
+        msg : exibirMsg,
+        produtos: produtos
+
+
+    });
+
 });
 
 app.listen(8181,function(erro){
